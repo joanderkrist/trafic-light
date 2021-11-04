4377fac85c0367d8f07a6d5d0a6c1f8a2a60b299
joanderkrist <83132937+joanderkrist@users.noreply.github.com>
2021-11-03 21:27:04 -0300

    no corresponde al proyecto

--- landinpage/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as card } from './card';
-export { default as data } from './data';
-export { default as footer } from './footer';
-export { default as jumbotron } from './jumbotron';
-export { default as navbar } from './navbar';
