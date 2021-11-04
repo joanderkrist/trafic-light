729d219da1e841f2fc4b36a47a51aad5257367e6
Kristian Zambrano <ing.kristianzambrano@gmail.com>
2021-11-03 21:03:19 -0300

    semaforo listo

---
+++ landinpage/src/components/data.js
@@ -0,0 +1,38 @@
+export let data = [
+    {
+      imgenUrl: "https://picsum.photos/id/237/400/300",
+      imgenAlt: "perro",
+      title: "Soy un Perro",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/236/400/300",
+      imgenAlt: "perro",
+      title: "Soy un Perro",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/238/400/300",
+      imgenAlt: "perro",
+      title: "Soy un Perro",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+    {
+      imgenUrl: "https://picsum.photos/id/235/400/300",
+      imgenAlt: "perro",
+      title: "Soy un Perro",
+      cardBody:
+        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+      btnUrl: "#",
+      btnLabel: "Go somewhere",
+    },
+  ];
---
+++ landinpage/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as card } from './card';
+export { default as data } from './data';
+export { default as footer } from './footer';
+export { default as jumbotron } from './jumbotron';
+export { default as navbar } from './navbar';
